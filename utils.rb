9ba57b8cdd657ff14fb319ec47338ace6cd22851
Bogdan <bogdan.mihai.timofte@gmail.com>
2017-07-11 21:30:55 +0300

    more validations

--- utils.rb
+++ utils.rb
@@ -8,6 +8,10 @@ module Utils
     position = Array(parts.last&.split(','))
     raise InvalidInstruction.new('Not valid coordinates') unless position.size == 3
 
+    raise InvalidInstruction.new('Not valid X coordinate') unless (0..4).include?(position[0].to_i)
+    raise InvalidInstruction.new('Not valid Y coordinate') unless (0..4).include?(position[1].to_i)
+    raise InvalidInstruction.new('Not valid facing') unless (0..4).include?(position[2].to_i)
+
     { x: position[0], y: position[1], facing: position[2] }
   end
 end
